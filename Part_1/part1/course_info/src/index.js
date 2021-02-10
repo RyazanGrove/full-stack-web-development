3c666a871b0c47e46783fe6b2c31d8d5870a46e7
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-02-10 14:03:00 +0200

    2.1: Course information step6

--- Part_1/part1/course_info/src/index.js
+++ Part_1/part1/course_info/src/index.js
@@ -14,9 +14,7 @@ const Part = (props) => (
 const Content = (props) => {
     return (
         <div>
-            <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}></Part>
-            <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}></Part>
-            <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}></Part>
+            {props.course.parts.map(course => <Part key={course.id} part={course.name} exercises={course.exercises}></Part>)}
         </div>
     )
 }
@@ -25,32 +23,37 @@ const Total = (props) => (
     <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
 )
 
+const Course = (props) => (
+    <div>
+        <Header course={props.course}></Header>
+        <Content course={props.course}></Content>
+    </div>
+)
+
 const App = () => {
     const course = {
+        id: 1,
         name: 'Half Stack application development',
         parts: [
             {
                 name: 'Fundamentals of React',
-                exercises: 130
+                exercises: 10,
+                id: 1
             },
             {
                 name: 'Using props to pass data',
-                exercises: 7
+                exercises: 7,
+                id: 2
             },
             {
                 name: 'State of a component',
-                exercises: 14
+                exercises: 14,
+                id: 3
             }
         ]
     }
 
-    return (
-        <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-        </div>
-    )
+    return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
