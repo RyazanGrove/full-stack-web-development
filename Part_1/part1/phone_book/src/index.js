bc7db26e84c9c6b24cefd105b174fd1a43abbcca
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-02-12 23:47:01 +0200

    2.9*: The Phonebook Step4

--- Part_1/part1/phone_book/src/index.js
+++ Part_1/part1/phone_book/src/index.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 import ReactDOM from "react-dom";
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-1234567' }
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNumber ] = useState('')
+    const [ useFilter, setUseFilter ] = useState(false)
+    const [personsToShow, setPersonsToShow] = useState(persons)
 
     const handleNameInputChange = (event) =>{
         setNewName(event.target.value)
@@ -14,6 +19,21 @@ const App = () => {
 
     const handleNumberInputChange = (event) =>{
         setNumber(event.target.value)
+    }
+
+    const filterNames = (event) =>{
+        if(event.target.value.length > 0){
+            let filteredNames = []
+            persons.map(person => {
+                if(person.name.toLowerCase().includes(event.target.value.toLowerCase())){
+                    filteredNames = filteredNames.concat(person)
+                }
+            })
+            setPersonsToShow(filteredNames)
+            setUseFilter(true)
+        } else {
+            setUseFilter(false)
+        }
     }
 
     const addPerson = (event) =>{
@@ -29,6 +49,10 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with <input onChange={filterNames}/>
+            </div>
+            <h2>Add a new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input onChange={handleNameInputChange}/>
@@ -41,7 +65,8 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+            {!useFilter && persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+            {useFilter && personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
         </div>
     )
 }
