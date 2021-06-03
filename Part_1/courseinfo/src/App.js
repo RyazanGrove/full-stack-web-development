ea1089c6f46afa94db88f4c2b7bd95682144a65e
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-06-03 18:12:09 +0300

    1.2: course information, step2 (new)

--- Part_1/courseinfo/src/App.js
+++ Part_1/courseinfo/src/App.js
@@ -6,19 +6,21 @@ const Header = (props) => (
     </div>
 )
 
-const Content = (props) => (
-    <div>
-      <p>
-        {props.parts[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.parts[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.parts[2]} {props.exercises[2]}
-      </p>
-    </div>
+const Part = (props) => (
+    <p>
+        {props.part} {props.exercises}
+    </p>
 )
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.parts[0]} exercises={props.exercises[0]}> </Part>
+            <Part part={props.parts[1]} exercises={props.exercises[1]}> </Part>
+            <Part part={props.parts[2]} exercises={props.exercises[2]}> </Part>
+        </div>
+    )
+}
 
 const Total = (props) => (
     <div>
