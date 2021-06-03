df403cc743ef9aa2ff8edc8d89100694070323e0
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-06-03 16:24:17 +0300

    1.1: course information, step1 (new)

---
+++ Part_1/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Header = (props) => (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+)
+
+const Content = (props) => (
+    <div>
+      <p>
+        {props.parts[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercises[2]}
+      </p>
+    </div>
+)
+
+const Total = (props) => (
+    <div>
+      <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    </div>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+      <div>
+        <Header course={course}> </Header>
+        <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}> </Content>
+        <Total exercises={[exercises1,exercises2,exercises3]}> </Total>
+      </div>
+  )
+}
+
+export default App
