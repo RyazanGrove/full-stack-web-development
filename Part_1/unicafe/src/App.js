d4189d4da19e946ab8a16e789c6e096995899f53
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-07-04 18:36:36 +0300

    1.11*: unicafe step6 (new)

--- Part_1/unicafe/src/App.js
+++ Part_1/unicafe/src/App.js
@@ -15,15 +15,15 @@ const Button = ({handleClick, text}) => {
 const Statistic = ({text, value}) =>{
     if(text === 'positive'){
         return(
-            <h3>{text} {value * 100} %</h3>
+            <tr><td>{text}</td><td>{value * 100} %</td></tr>
         )
     }
     return(
-        <h3>{text} {value}</h3>
+        <tr><td>{text}</td><td>{value}</td></tr>
     )
 }
 
-const Statistics = ({title, good, neutral, bad}) =>{
+const Statistics = ({good, neutral, bad}) =>{
     //single component to display statistics
     const totalNumber = good + neutral + bad
     if(totalNumber === 0){
@@ -32,15 +32,16 @@ const Statistics = ({title, good, neutral, bad}) =>{
         )
     }
     return(
-        <div>
-            <h1>{title}</h1>
-            <Statistic text='good' value={good}/>
-            <Statistic text='neutral' value={neutral}/>
-            <Statistic text='bad' value={bad}/>
-            <Statistic text='all' value={totalNumber}/>
-            <Statistic text='average' value={(good - bad)/totalNumber}/>
-            <Statistic text='positive' value={good / totalNumber}/>
-        </div>
+        <table>
+            <tbody>
+                <Statistic text='good' value={good}/>
+                <Statistic text='neutral' value={neutral}/>
+                <Statistic text='bad' value={bad}/>
+                <Statistic text='all' value={totalNumber}/>
+                <Statistic text='average' value={(good - bad)/totalNumber}/>
+                <Statistic text='positive' value={good / totalNumber}/>
+            </tbody>
+        </table>
     )
 }
 
@@ -56,7 +57,8 @@ const App = () => {
           <Button handleClick={()=>setGood(good + 1)} text='good'/>
           <Button handleClick={()=>setNeutral(neutral + 1)} text='neutral'/>
           <Button handleClick={()=>setBad(bad + 1)} text='bad'/>
-          <Statistics title='statistics' good={good} neutral={neutral} bad={bad}/>
+          <h1>Statistics</h1>
+          <Statistics good={good} neutral={neutral} bad={bad}/>
       </div>
   )
 }
