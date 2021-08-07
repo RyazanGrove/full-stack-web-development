7892f6b064e2d1cfa85719bb35ba83dde50f9f26
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-08-07 20:52:58 +0300

    2.13*: Data for countries, step2 (new)

--- Part_2/data_for_countries/src/components/Country.js
+++ Part_2/data_for_countries/src/components/Country.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Country = ({isSingle, country}) => {
+    const [viewIsShown ,setViewIsShown] = useState(false)
+
+    const showView = () => {
+        setViewIsShown(true)
+    }
+
     if(isSingle === false){
-        return (
-            <div>{country.name}</div>
-        )
+        if(viewIsShown === true){
+            return(
+                <div>
+                    <h2>{country.name}</h2>
+                    <p>capital {country.capital}</p>
+                    <p>population {country.population}</p>
+                    <h3>languages</h3>
+                    <ul>{country.languages.map(language => <li key={language.name}>{language.name}</li>)}</ul>
+                    <img src={country.flag} width={200} height={200}/>
+                </div>
+            )
+        }else {
+            return (
+                <div>
+                    {country.name}<button onClick={showView}>show</button>
+                </div>
+            )
+        }
     } else {
         country = country[0]
         return (
