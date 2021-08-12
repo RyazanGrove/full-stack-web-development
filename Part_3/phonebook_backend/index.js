94837fb3e57867e53a3413c2a739994ed145338e
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-08-12 23:45:58 +0300

    3.6: Phonebook backend step6

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -73,9 +73,21 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
+    if (!body.number) {
         return response.status(400).json({
-            error: 'content missing'
+            error: 'number missing'
+        })
+    }
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (persons.filter(person => person.name === body.name).length > 0) {
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
 
