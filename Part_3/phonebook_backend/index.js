566d763dc7cebcdffc7da6db2b0ca4f501fc76cf
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-08-12 23:32:58 +0300

    3.4: Phonebook backend step4

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -54,6 +54,7 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+//delete entry
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
