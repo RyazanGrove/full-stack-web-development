c1219d9c44d9acd1f9e54385904621fbb55bae56
RyazanGrove <RyazanGrove>
2021-08-15 02:19:29 +0300

    uibuild

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -1,115 +1,118 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(express.json())
-morgan.token('body', (req) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(cors())
+app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+app.use(cors());
+app.use(express.static("build"));
 
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/info',(request, response) => {
-    const entries = `Phonebook has info for ${persons.length} people`
-    const timestamp = new Date();
-    const timeStr = `${timestamp.toString()}`
-    const res = entries + '<br>' + timeStr
-    response.send(res)
-})
+app.get("/info", (request, response) => {
+  const entries = `Phonebook has info for ${persons.length} people`;
+  const timestamp = new Date();
+  const timeStr = `${timestamp.toString()}`;
+  const res = entries + "<br>" + timeStr;
+  response.send(res);
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
 //single entry
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
 //delete entry
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
 
-    response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const generateId = () => {
-    let randomNumber = Math.round(Math.random() * 1000000)
-    while((persons.filter(person => person.id === randomNumber)).length > 0) {
-        randomNumber = Math.round(Math.random() * 1000000)
-    }
-    return randomNumber
-}
+  let randomNumber = Math.round(Math.random() * 1000000);
+  while (persons.filter((person) => person.id === randomNumber).length > 0) {
+    randomNumber = Math.round(Math.random() * 1000000);
+  }
+  return randomNumber;
+};
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
 
-    if (persons.filter(person => person.name === body.name).length > 0) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+  if (persons.filter((person) => person.name === body.name).length > 0) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
 
-    persons = persons.concat(person)
+  persons = persons.concat(person);
 
-    response.json(person)
-})
+  response.json(person);
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
