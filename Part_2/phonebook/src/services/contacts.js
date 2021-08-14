96445c530e862123f5b6e0c5b84356b3bb456b80
Mikhail Raskhodchikov <mikhail.raskhodchikov@flowhavem.com>
2021-08-15 01:32:07 +0300

    build production version

--- Part_2/phonebook/src/services/contacts.js
+++ Part_2/phonebook/src/services/contacts.js
@@ -1,30 +1,30 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+import axios from "axios";
+const baseUrl = "api/persons";
 
 const getAll = () => {
-    return axios.get(baseUrl)
-}
+  return axios.get(baseUrl);
+};
 
 const get = (id) => {
-    return axios.get(`${baseUrl}/${id}`)
-}
+  return axios.get(`${baseUrl}/${id}`);
+};
 
-const create = newObject => {
-    return axios.post(baseUrl, newObject)
-}
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
 
 const remove = (i) => {
-    return axios.delete(`${baseUrl}/${i}`)
-}
+  return axios.delete(`${baseUrl}/${i}`);
+};
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
 export default {
-    getAll: getAll,
-    create: create,
-    remove: remove,
-    update: update,
-    get: get
-}
+  getAll: getAll,
+  create: create,
+  remove: remove,
+  update: update,
+  get: get,
+};
