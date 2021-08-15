120561a0033d09d990a62ad898330aeb9f8eec9e
RyazanGrove <RyazanGrove>
2021-08-15 03:06:19 +0300

    uibuild

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -35,7 +35,7 @@ let persons = [
 ];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!3</h1>");
+  response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (request, response) => {
